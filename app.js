f738c612d983e43599afb22eac52cb438f3fe304
MKAbdElrahman <smohamed.abdelrahman@gmail.com>
2023-07-01 05:25:56 +0300

    implemented a hello session router

--- app.js
+++ app.js
@@ -7,21 +7,32 @@ const path = require("path")
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+const sessionRouter = express.Router();
 
 
 app.use(morgan("tiny"));
 
 
-app.use(express.static(path.join(__dirname,"/public")));
-app.set("views","./src/views");
-app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname, "/public")));
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
 
-app.get("/",(req,res)=>{
-    res.render("index",{title: "Globomantics",data:['a','b','c']});
+sessionRouter.route("/").get((req, res) => {
+    res.send("hello sessions");
+})
+sessionRouter.route("/1").get((req, res) => {
+    res.send("hello single sessions");
+})
+app.use("/sessions", sessionRouter);
+app.get("/", (req, res) => {
+    res.render("index", {
+        title: "Globomantics",
+        data: ['a', 'b', 'c']
+    });
 })
 
 
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     debug(`listening on port ${chalk.green(PORT)}`);
 });
