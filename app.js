f10b0f89c82c0607402e38391080dec26abc23b0
MKAbdElrahman <smohamed.abdelrahman@gmail.com>
2023-07-01 05:56:14 +0300

    passed session objects

--- app.js
+++ app.js
@@ -18,7 +18,13 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs");
 
 sessionRouter.route("/").get((req, res) => {
-    res.send("hello sessions");
+    res.render("sessions",{sessions:[
+        {title:"Session 1",description: "this is session 1"},
+        {title:"Session 2",description: "this is session 2"},
+        {title:"Session 3",description: "this is session 3"},
+        {title:"Session 4",description: "this is session 4"}
+
+    ]});
 })
 sessionRouter.route("/1").get((req, res) => {
     res.send("hello single sessions");
